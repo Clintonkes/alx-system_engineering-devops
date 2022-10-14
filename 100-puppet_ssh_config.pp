92320080208819ee29c8384d292d8cff31da653d
Clintonkes <clintonemeka05@gmail.com>
2022-10-14 10:53:59 +0000

    Client configuration using ssh

---
+++ 100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# set the ssh for the config file
+file_line { 'Change the main private key':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
+}
+
+file_line { 'No Authenticate with passowrd':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+}
